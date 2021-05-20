e7922b1e3c8eaf20ddb85a00274a480337c56b1b
Israel Miller <ismille@microsoft.com>
2021-05-20 12:02:48 -0400

    update for successful tests

--- AzureSqlMSIAllower/Program.cs
+++ AzureSqlMSIAllower/Program.cs
@@ -9,15 +9,12 @@ namespace AzureSqlMSIAllower
     class Program
     {
         private const string allowCmd = @"
-IF (SELECT 1 from sys.database_principals where name = [{0}])
-BEGIN
-DECLARE @cmd nvarchar(4000)
-SET @cmd = 'CREATE USER [{0}] WITH SID = '+CONVERT(VARCHAR(1000), CAST(CAST('{1}' AS UNIQUEIDENTIFIER) AS varbinary(16)), 1)+', TYPE=E'
-EXEC sp_executesql @cmd
-ALTER ROLE db_owner ADD MEMBER [{0}]
-RETURN 0
-END
-RETURN 1";
+IF NOT EXISTS (SELECT 1 from sys.database_principals where name = '{0}')
+    DECLARE @cmd nvarchar(4000);
+    SET @cmd = 'CREATE USER [{0}] WITH SID = '+CONVERT(VARCHAR(1000), CAST(CAST('{1}' AS UNIQUEIDENTIFIER) AS varbinary(16)), 1)+', TYPE=E';
+    EXEC sp_executesql @cmd;
+    ALTER ROLE db_owner ADD MEMBER [{0}];
+SELECT COUNT(*) from sys.database_principals where name = '{0}';";
 
         static int Main(string[] args)
         {
@@ -34,18 +31,36 @@ RETURN 1";
 
             foreach (KeyValuePair<string, Dictionary <string, string>> db in sqlConfig.databases)
             {
-
-                using (SqlConnection sqlConn = new SqlConnection(string.Format("Server={0};Authentication=Active Directory MSI;Database={1};UID={2};", sqlConfig.server, db.Key, sqlConfig.msiClientId)))
+                foreach (KeyValuePair<string, string> msi in db.Value)
                 {
-                    foreach (KeyValuePair<string, string> msi in db.Value)
+                    using (SqlConnection sqlConn = new SqlConnection(string.Format("Server={0};Authentication=Active Directory MSI;Database={1};UID={2};", sqlConfig.server, db.Key, sqlConfig.msiClientId)))
                     {
                         string commandTxt = string.Format(allowCmd, msi.Key, msi.Value);
                         using (SqlCommand command = new SqlCommand(commandTxt, sqlConn))
                         {
                             sqlConn.Open();
-                            var returnCode = (int) command.ExecuteScalar();
-                            if (returnCode != 0) {
-                                Console.WriteLine(string.Format("MSI {0} is already added to the DB", msi.Key));
+                            int returnCode;
+                            try
+                            {
+                                returnCode = (int)command.ExecuteScalar();
+                                if (returnCode != 0)
+                                {
+                                    Console.WriteLine(string.Format("MSI {0} is added to the DB", msi.Key));
+                                }
+
+                            } catch (SqlException e)
+                            {
+                                if (e.Number == 15063)
+                                {
+                                    Console.WriteLine(string.Format("MSI {0} is already added to the DB under a different username", msi.Key));
+                                } else
+                                {
+                                    Console.WriteLine("Encountered an error on SQL execution.");
+                                    Console.WriteLine(e.ToString());
+                                }
+                            } finally
+                            {
+                                sqlConn.Close();
                             }
                         }
                     }
